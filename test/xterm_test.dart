35b0a3c5c5183d4032968d86cb8b0c6568b66c44
xuty <xty50337@hotmail.com>
2021-04-13 16:01:18 +0800

    setup github actions

--- test/xterm_test.dart
+++ test/xterm_test.dart
@@ -1,13 +1,10 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:xterm/xterm.dart';
+import 'package:xterm/xterm.dart';
 
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//     expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-//   });
-// }
+void main() {
+  test('Can instantiate Terminal', () {
+    final terminal = Terminal(maxLines: 10000);
+    terminal.write('hello');
+  });
+}
