7fc229fbe43204d56fbe88665d049359a8cc5ea9
stalin <stalin@tencent.com>
2021-09-26 16:07:52 +0800

    enable backspace for mobile platform. I think it's bad idea to totally reject key stroke. It made many functional keybinding not usable. Just accept it as PC does and throw those situation to application developer is more resonable.

--- lib/frontend/input_behavior_mobile.dart
+++ lib/frontend/input_behavior_mobile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:xterm/frontend/input_behavior_default.dart';
 import 'package:xterm/input/keys.dart';
 import 'package:xterm/xterm.dart';
@@ -7,30 +6,10 @@ import 'package:xterm/xterm.dart';
 class InputBehaviorMobile extends InputBehaviorDefault {
   InputBehaviorMobile();
 
-  final acceptKeyStroke = false;
+  @override
+  final acceptKeyStroke = true;
 
-  final initEditingState = const TextEditingValue(
-    text: '  ',
-    selection: TextSelection.collapsed(offset: 1),
-  );
-
-  TextEditingValue onTextEdit(
-      TextEditingValue value, TerminalUiInteraction terminal) {
-    if (value.text.length > initEditingState.text.length) {
-      terminal.raiseOnInput(value.text.substring(1, value.text.length - 1));
-    } else if (value.text.length < initEditingState.text.length) {
-      terminal.keyInput(TerminalKey.backspace);
-    } else {
-      if (value.selection.baseOffset < 1) {
-        terminal.keyInput(TerminalKey.arrowLeft);
-      } else if (value.selection.baseOffset > 1) {
-        terminal.keyInput(TerminalKey.arrowRight);
-      }
-    }
-
-    return initEditingState;
-  }
-
+  @override
   void onAction(TextInputAction action, TerminalUiInteraction terminal) {
     print('action $action');
     switch (action) {
