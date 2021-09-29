c7ba8be0076dd2915846dae689b4661ac45a27ab
stalin <stalin@tencent.com>
2021-09-29 09:57:00 +0800

    add oTextEdit back

--- lib/frontend/input_behavior_mobile.dart
+++ lib/frontend/input_behavior_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:xterm/frontend/input_behavior_default.dart';
 import 'package:xterm/input/keys.dart';
 import 'package:xterm/xterm.dart';
@@ -6,10 +7,30 @@ import 'package:xterm/xterm.dart';
 class InputBehaviorMobile extends InputBehaviorDefault {
   InputBehaviorMobile();
 
-  @override
   final acceptKeyStroke = true;
 
-  @override
+  final initEditingState = const TextEditingValue(
+    text: '  ',
+    selection: TextSelection.collapsed(offset: 1),
+  );
+
+  TextEditingValue onTextEdit(
+      TextEditingValue value, TerminalUiInteraction terminal) {
+    if (value.text.length > initEditingState.text.length) {
+      terminal.raiseOnInput(value.text.substring(1, value.text.length - 1));
+    } else if (value.text.length < initEditingState.text.length) {
+      terminal.keyInput(TerminalKey.backspace);
+    } else {
+      if (value.selection.baseOffset < 1) {
+        terminal.keyInput(TerminalKey.arrowLeft);
+      } else if (value.selection.baseOffset > 1) {
+        terminal.keyInput(TerminalKey.arrowRight);
+      }
+    }
+
+    return initEditingState;
+  }
+
   void onAction(TextInputAction action, TerminalUiInteraction terminal) {
     print('action $action');
     switch (action) {
