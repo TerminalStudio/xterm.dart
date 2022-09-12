2b88918b1871c46c10655ad951c66fa04ed4d6e5
xuty <xty50337@hotmail.com>
2022-09-12 21:31:44 +0800

    Remove unused import

--- test/src/terminal_view_test.dart
+++ test/src/terminal_view_test.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:xterm/xterm.dart';
